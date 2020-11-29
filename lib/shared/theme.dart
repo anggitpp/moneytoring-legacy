2eb17a46e57ed429ccb7eda7a823566c69b225e2
Anggit <anggitpp23@gmail.com>
2020-11-29 14:33:34 +0700

    penerapan mvc dan pembuatan variable untuk project

---
+++ lib/shared/theme.dart
@@ -0,0 +1,13 @@
+part of 'shared.dart';
+
+double defaultMargin = 26;
+
+Color mainColor = "#FFC801".toColor();
+Color backColor = "#FAFAFA".toColor();
+Color greyColor = "#E5E5E5".toColor();
+
+TextStyle smallFont = GoogleFonts.poppins(fontSize: 12, color: Colors.black);
+TextStyle mediumFont = GoogleFonts.poppins(fontSize: 15, color: Colors.black);
+TextStyle largeFont = GoogleFonts.poppins(fontSize: 20, color: Colors.black);
+TextStyle greyFont =
+    GoogleFonts.poppins(fontSize: 12, color: "#6C6C6C".toColor());
