bd3e6920641479abf734ee1496da23c8e6843623
Anggit <anggitpp23@gmail.com>
2020-11-29 14:46:01 +0700

    membuat splash page

--- lib/view/pages/pages.dart
+++ lib/view/pages/pages.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:moneytoring/shared/shared.dart';
 
 part 'splash_page.dart';
--- lib/view/pages/splash_page.dart
+++ lib/view/pages/splash_page.dart
@@ -3,6 +3,46 @@ part of 'pages.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: backColor,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: backColor,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Moneytoring',
+                style: largeFont.copyWith(
+                    fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+                child: Text(
+                  'Monitoring your money now\neasier than before.',
+                  textAlign: TextAlign.center,
+                  style: greyFont.copyWith(fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Image(
+                image: AssetImage('assets/splash_image.png'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
